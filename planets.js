5f8a5feb67ab8510511124e83144d73299dcd03f
anthony@appcard.com <--global>
2014-05-05 13:55:34 -0400

    planets

---
+++ planets.js
@@ -0,0 +1,19 @@
+// Planetary objects.
+
+planets = {
+  0: {
+    'name': 'Earth',
+    'charAttrs': {
+       gold: 1,
+       initialGoldPerSec: 1,
+       goldPerSec: 1,
+       resources: ["Oxygen", "Iron", "Fuel"]
+    },
+    1: {
+        'name': 'the Moon',
+        gold: 0,
+        initialGoldPerSec: 0,
+        resources: ["Helium-3", "Aluminum", "Magnesium"]
+    }
+  },
+}
