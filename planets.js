44a50447dd6cd77406dd7b4f52edae363ad40526
CookingASCII <xavier1999@videotron.ca>
2014-05-05 13:59:49 -0400

    Edition of planet.js
    You forgot a thing at the end :3

--- planets.js
+++ planets.js
@@ -1,19 +1,20 @@
 // Planetary objects.
 
 planets = {
-  0: {
-    'name': 'Earth',
-    'charAttrs': {
-       gold: 1,
-       initialGoldPerSec: 1,
-       goldPerSec: 1,
-       resources: ["Oxygen", "Iron", "Fuel"]
+    0: {
+        'name': 'Earth',
+        'charAttrs': {
+        gold: 1,
+        initialGoldPerSec: 1,
+        goldPerSec: 1,
+        resources: ["Oxygen", "Iron", "Fuel"]
     },
     1: {
         'name': 'the Moon',
         gold: 0,
         initialGoldPerSec: 0,
+		goldPerSec: 0,
         resources: ["Helium-3", "Aluminum", "Magnesium"]
     }
   },
-}
+};
