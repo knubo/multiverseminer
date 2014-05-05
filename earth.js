2b4d472566726f8276b36db15d9ab17c992d0b14
anthony@appcard.com <--global>
2014-05-05 16:56:33 -0400

    Earth

---
+++ earth.js
@@ -0,0 +1,10 @@
+earth = {
+    "name": "Earth",
+    "layers": {
+        "0": ["Atmosphere", 0, 0, ["Oxygen"]],
+        "1": ["Crust", 0, 35, ["Iron", "Gold"]],
+        "2": ["Upper Mantle", 35, 60, ["Iron", "Fuel"]],
+        "3": ["Mantle", 60, 2890, ["Iron"]]
+    },
+    
+}
