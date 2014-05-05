d103f8e388106fd955e90ddc4fcdb0ce0b873558
anthony@appcard.com <--global>
2014-05-05 14:08:06 -0400

    TODO

--- planets.js
+++ planets.js
@@ -1,7 +1,6 @@
 // Planetary objects.
 
 planets = {
-<<<<<<< HEAD
   0: {
     'name': 'Earth',
     'charAttrs': {
@@ -9,28 +8,14 @@ planets = {
        initialGoldPerSec: 1,
        goldPerSec: 1,
        resourceAboveGround: ["Oxygen"],
-       resourceUnderGround: ["Iron", "Fuel"],
-=======
-    0: {
-        'name': 'Earth',
-        'charAttrs': {
-        gold: 1,
-        initialGoldPerSec: 1,
-        goldPerSec: 1,
-        resources: ["Oxygen", "Iron", "Fuel"]
->>>>>>> 44a50447dd6cd77406dd7b4f52edae363ad40526
+       resourceUnderGround: ["Iron", "Fuel"]
     },
     1: {
         'name': 'the Moon',
         gold: 0,
         initialGoldPerSec: 0,
-<<<<<<< HEAD
         resourceAboveGround: ["Helium-3"],
         resourceUnderGround: ["Aluminum", "Magnesium"]
-=======
-		goldPerSec: 0,
-        resources: ["Helium-3", "Aluminum", "Magnesium"]
->>>>>>> 44a50447dd6cd77406dd7b4f52edae363ad40526
     }
   },
-};
+}
