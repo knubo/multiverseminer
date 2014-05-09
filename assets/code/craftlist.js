fc4709206a6ef3be3b6147e99f86c22d417ad769
CookingASCII <xavier1999@videotron.ca>
2014-05-09 18:01:18 -0400

    Added craftlist.js
    Feel dree to add next crafts and bind numbers to object

---
+++ assets/code/craftlist.js
@@ -0,0 +1,19 @@
+//0 = empty
+
+var crafts = {
+    0: {
+	    name: "Copper Bar",
+		info: "",
+		recipe: [[1, 1, 1], [1, 1, 1],[1, 1, 1]]
+	},
+	1: {
+	    name: "Iron Bar",
+		info: "",
+		recipe: [[2, 2, 2], [2, 2, 2],[2, 2, 2]]
+	},
+	2: {
+	    name: "Gold Bar",
+		info: "",
+		recipe: [[3, 3, 3], [3, 3, 3],[3, 3, 3]]
+	},
+};
