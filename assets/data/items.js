ac25885712648cddbe03733a36b7d53b762eed72
anthony@appcard.com <--global>
2014-05-14 12:21:50 -0400

    removed 2 icons

--- assets/data/items.js
+++ assets/data/items.js
@@ -133,7 +133,6 @@ addItem(5, 'gold', 'Gold', 100, {
 });
 
 addItem(6, 'silicon', 'Silicon', 1000, {
-        'icon': sys.iconRoot + 'silicon.png',
     'el': 'Si'
 });
 
@@ -143,7 +142,6 @@ addItem(7, 'aluminum', 'Aluminum', 1000, {
 });
 
 addItem(8, 'calcium', 'Calcium', 1000, {
-    'icon': sys.iconRoot + 'calcium.png',
     'el': 'Ca'
 });
 
