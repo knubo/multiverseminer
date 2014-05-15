44faccedcff106b335ff2bcf21cf510717a6717d
Craiel <loft45@gmail.com>
2014-05-14 22:15:51 -0400

    minor fixes
    - made most divs and elements non-selectable
    - adjusted css a little to scale down further and look decent on smaller
    screens and firefox
    - added select first / last in the selection control
    - adjusted small grey planet image slightly
    - refactored ui selection to pass self in instead of using an array /
    key lookup

--- assets/data/system.js
+++ assets/data/system.js
@@ -7,6 +7,12 @@ function System() {
     this.imageRoot = "assets/images/";
     this.iconRoot = this.imageRoot + "itemIcons/";
     
+    // Selection control
+    this.selectionArrowBack = this.imageRoot + 'selectionArrowBack.png';
+    this.selectionArrowBackFast = this.imageRoot + 'selectionArrowBackFast.png';
+    this.selectionArrowForward = this.imageRoot + 'selectionArrowForward.png';
+    this.selectionArrowForwardFast = this.imageRoot + 'selectionArrowForwardFast.png';
+    
     // Icons
     this.iconPlaceholder = this.iconRoot + 'icon_placeholder.png';
     this.iconPlaceholderRawMaterial = this.iconRoot + 'icon_placeholderRawMaterial.png';
