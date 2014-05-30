42c50598a86d91c5224263f9dda6f4f14b97a38d
anthony@appcard.com <--global>
2014-05-30 18:41:08 -0400

    whoops

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,9 +48,11 @@ module.exports = function(grunt) {
 						uitravelscreen : 'ui/uiTravelScreen',
 
 						jquery    : 'external/jquery-2.1.1.min',
+						
 						jqueryui  : 'external/jquery-ui-1.10.4.custom.min',
 						jgrowl    : 'external/jquery.jgrowl.min',
-						starfield : 'external/starfield'
+						starfield : 'external/starfield',
+						powertip  : 'external/jquery.powertip.min'
 					}
 				}
 			}
@@ -109,3 +111,4 @@ module.exports = function(grunt) {
 		//'dataExport'
 	]);
 };
+
