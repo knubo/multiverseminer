0dbfa603015e847808d18c7f4c904ec3c8cce049
antlong@gmail.com <--global>
2014-06-04 18:49:33 -0400

    not done with chat yet

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,4 +1,4 @@
-require([ "uiplanetscreen", "uitravelscreen", "uifloating", "jquery", "jqueryui", "jgrowl", "powertip", "vex" ]);
+require([ "uiplanetscreen", "uitravelscreen", "uifloating", "jquery", "jqueryui", "jgrowl", "powertip" ]);
 
 // ---------------------------------------------------------------------------
 // Some const values used in ui code
