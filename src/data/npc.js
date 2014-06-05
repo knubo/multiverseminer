36fd0ab92dd9cdc3baf1079d278947b3d03293f2
antlong@gmail.com <--global>
2014-06-04 23:12:08 -0400

    someone whos name starts with a C broke pickaxes

---
+++ src/data/npc.js
@@ -0,0 +1,67 @@
+// ------------------------------------------------------------------------
+// AUTO-GENERATED, DO NOT EDIT MANUALLY
+//  Generated @ Wed Jun 04 2014 23:11:24 GMT-0400 (EDT)
+// ------------------------------------------------------------------------
+
+NPC = {
+	// -------------------------------------------
+	// npc.xls
+	// -------------------------------------------
+	'policeMan': {
+		'id': 'policeMan',
+		'name': 'Police Man',
+		'category': 'enemy',
+		'description': 'An angry policeman',
+		'attackType': 'hit',
+		'attackText': ' whacks you with a ',
+		'weaponName': 'baton',
+		'strength': 1,
+		'accuracy': 2,
+		'defense': 1,
+		'evasion': 1,
+		'attackSpeed': 1,
+		'shipSpeed': 1,
+		'health': 1,
+		'miningLuck': 1,
+		'scavengeluck': 1,
+		'lootluck': 1,
+		'counter': 1,
+		'regeneration': 1,
+		'resillience': 1,
+		'perception': 1,
+		'healChance': 0.4,
+		'xpGain': 100,
+		'lootTable': 'npcGenericLoot',
+		'winText': 'Protect and Serve!',
+		'loseText': 'Calling for backup!',
+	},
+	'thug': {
+		'id': 'thug',
+		'name': 'A thug',
+		'category': 'enemy',
+		'description': 'An angry thug',
+		'attackType': 'hit',
+		'attackText': ' slaps you with a ',
+		'weaponName': 'blackjack',
+		'strength': 2,
+		'accuracy': 2,
+		'defense': 1,
+		'evasion': 1,
+		'attackSpeed': 1,
+		'shipSpeed': 1,
+		'health': 1,
+		'miningLuck': 1,
+		'scavengeluck': 1,
+		'lootluck': 1,
+		'counter': 1,
+		'regeneration': 1,
+		'resillience': 1,
+		'perception': 1,
+		'healChance': 0.1,
+		'xpGain': 100,
+		'lootTable': 'npcGenericLoot',
+		'winText': 'SUCKA!',
+		'loseText': 'AWW SHIT!',
+	},
+
+}
