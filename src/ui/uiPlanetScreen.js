434a26503c2bd2e9fe1a55586b21660a8a169466
antlong@gmail.com <--global>
2014-06-09 23:36:48 -0400

    working on dimming

--- src/ui/uiPlanetScreen.js
+++ src/ui/uiPlanetScreen.js
@@ -335,8 +335,8 @@ function UIPlanetScreen() {
         $("#playerCraftingContent").accordion({
             heightStyle: "content"
         });
-        var activePage = $('#playerCraftingContent').accordion('option', 'active');
-        $("#playerCraftingContent").accordion('option', 'active', activePage);
+        //var activePage = $('#playerCraftingContent').accordion('option', 'active');
+        //$("#playerCraftingContent").accordion('option', 'active', activePage);
     };
 
     this.updateEmpirePanel = function() {
