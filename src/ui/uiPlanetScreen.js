ad072473113866a37599b1d3642a419e7d3f48d0
antlong@gmail.com <--global>
2014-06-13 15:31:42 -0400

    A better place to begin at.

--- src/ui/uiPlanetScreen.js
+++ src/ui/uiPlanetScreen.js
@@ -219,7 +219,7 @@ function UIPlanetScreen() {
     this.updatePlayerInventoryPanel = function() {
         var self = ui.screenPlanet;
  
-        // self.playerInventory.update(game.player.storage);
+        //self.playerInventory.update(game.player.storage);
     };
  
     this.updatePlanetInventoryPanel = function() {
