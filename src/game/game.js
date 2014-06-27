a9cfe72c7ffab74134c482ce89fa7a786d01b383
antlong@gmail.com <--global>
2014-06-27 12:58:24 -0400

    maybe this.update() instead

--- src/game/game.js
+++ src/game/game.js
@@ -1,5 +1,4 @@
-require(["gameplayer", "gameplanet", "gamenpc", "gamefight", "gamesettings", "gamestatistics", "utils", "gamequest", "noty", "uiplanetscreen"]);
-var uiplanetscreen = new UIPlanetScreen();
+require(["gameplayer", "gameplanet", "gamenpc", "gamefight", "gamesettings", "gamestatistics", "utils", "gamequest", "noty"]);
 
 function Game() {
     this.player = new Player();
@@ -373,7 +372,7 @@ function Game() {
         }
         this.currentPlanet = this.planetDictionary[target.id];
         this.planetChanged = true;
-        uiplanetscreen.activatePlanet;
+        this.update();
         //this.currentPlanet.load();
         
     };
