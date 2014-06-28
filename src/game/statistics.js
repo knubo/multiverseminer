10fe4b421d535d3ca0758e42e7dece8fcd1436e3
antlong@gmail.com <--global>
2014-06-28 09:21:05 -0400

    Removed unneeded js files

--- src/game/statistics.js
+++ src/game/statistics.js
@@ -7,6 +7,7 @@ function Statistics(key) {
     this.autoscavengeCount = "0";
     this.autogatherCount = "0";
     this.autorefineCount = "0";
+    this.foundItems = "0";
 
     // ---------------------------------------------------------------------------
     // general
@@ -32,6 +33,7 @@ function Statistics(key) {
         localStorage[storageKey + 'autogatherCount'] = this.autogatherCount;
         localStorage[storageKey + 'autoscavengeCount'] = this.autoscavengeCount;
         localStorage[storageKey + 'autorefineCount'] = this.autorefineCount;
+        localStorage[storageKey + 'foundItems'] = this.foundItems;
 
     };
 
@@ -44,6 +46,7 @@ function Statistics(key) {
         this.autogatherCount = utils.loadInt(storageKey + 'autogatherCount', "0");
         this.autoscavengeCount = utils.loadInt(storageKey + 'autoscavengeCount', "0");
         this.autorefineCount = utils.loadInt(storageKey + 'autorefineCount', "0");
+        this.foundItems = utils.loadInt(storageKey + 'foundItems', "0");
     };
 
     this.reset = function(fullReset) {
@@ -54,5 +57,6 @@ function Statistics(key) {
         localStorage.setValue(this.autogatherCount, "0");
         localStorage.setValue(this.autoscavengeCount, "0");
         localStorage.setValue(this.autorefineCount, "0");
+        localStorage.setValue(this.foundItems, "0");
     };
 };
